bb938b5da09528dde4d67e1ffc677fb5e7bee175
senanayakes <senanayakes@luxbet.com>
2015-10-22 09:06:22 +1100

    added feed venue priority model class

---
+++ service/models/feedVenuePriority.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Name goes here
+ * Description goes here
+ *
+ * @author senanayakes
+ * @package
+ * @subpackage
+ *
+ * @copyright Copyright © Tabcorp Pty Ltd. All rights reserved. http://www.tabcorp.com.au/
+ * @license This code is copyrighted and is the exclusive property of Tabcorp Pty Ltd. It may not be used, copied or redistributed without the written permission of Tabcorp.
+ */
+
+use Phalcon\Mvc\Model;
+
+class feedVenuePriority {
+
+	public function getSource()
+	{
+		return 'feed_venue_priority'; // name of db table here
+	}
+
+}
